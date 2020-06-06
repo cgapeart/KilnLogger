01c71e3273d655ef00bbf05bf2d7bde067747eb9
Colin Peart <cgapeart@gmail.com>
2020-06-06 14:21:33 -0600

    Initial Commit

---
+++ KilnLogger.ino
@@ -0,0 +1,295 @@
+#include <ESPPerfectTime.h>
+#include <sntp.h>
+#include <LittleFS.h>
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#include "Adafruit_MAX31856.h"
+
+#ifndef STASSID
+#define STASSID "YOUR_WIFI_SSID"
+#define STAPSK  "YOUR_WIFI_PASSWORD"
+#define STANAME "mDNS_Name_to_broadcast"
+#endif
+
+const uint8_t xCS = D3;
+const uint8_t xSDI = D2;
+const uint8_t xSDO = D1;
+const uint8_t xSCK = D0;
+const uint8_t xFLT = D5;
+const uint8_t xDRD = D6;
+const char *ssid = STASSID;
+const char *password = STAPSK;
+const char *mdnsName = STANAME;
+
+struct sample_t
+{
+  time_t time;
+  float outsideTemp;
+  float insideTemp;
+};
+#define BUF_LEN 20
+volatile int current = 0;
+sample_t buffer[BUF_LEN] = {0};
+#define SAMPLE_PERIOD 10000
+
+#define RESP_BUFFER_LEN 24000
+
+ESP8266WebServer server(80);
+
+Adafruit_MAX31856 tempSensor(xCS,xSDI,xSDO,xSCK);
+
+void handleRoot() {
+  
+  char temp[RESP_BUFFER_LEN];
+  int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
+
+  
+  struct tm *localTime = pftime::localtime(nullptr);
+  
+  snprintf(temp, RESP_BUFFER_LEN,
+            "<html>\n"
+              "<head>\n"
+                //"<meta http-equiv='refresh'>\n"
+                "<title>Internet Of Kilns</title>\n"
+                "<link rel=\"stylesheet\" href=\"/chartist.min.css\">\n"
+                "<style>\n"
+                  "body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\n"
+                "</style>\n"
+              "</head>\n"
+              "<body>\n"
+                "<p>Uptime: %02d:%02d:%02d</p>\n"
+                "<p>Date and time: %04d-%02d-%02d %02d:%02d:%02d</p>\n"
+                "<p>Outside Temp:%f degrees C</p>\n"
+                "<p>Measured Temp:%f degrees C</p>\n"
+                "<div class=\"ct-chart ct-perfect-fourth\"></div>\n"
+                "<script src=\"/chartist.min.js\"></script>\n"
+                "<script src=\"/data.js\"/></script>\n"
+                "<script>\n"
+                  "new Chartist.Line('.ct-chart', data);\n"
+                "</script>\n"
+                "OK\n"
+              "</body>\n"
+            "</html>\n",
+           hr, min % 60, sec % 60,
+           localTime->tm_year + 1900, localTime->tm_mon + 1, localTime->tm_mday,
+           localTime->tm_hour, localTime->tm_min, localTime->tm_sec,
+           buffer[current].outsideTemp,buffer[current].insideTemp);
+
+  //server.sendHeader("Cache-Control", "max-age=3600", true);
+  server.sendHeader("Cache-Control", "no-cache", true);
+  server.send(200, "text/html", temp);
+  
+}
+
+void handleNotFound() {
+  if(handleFileRead(server.uri()))
+  {
+    return;
+  }
+
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+
+  server.send(404, "text/plain", message);
+  
+}
+
+String getContentType(String filename) { // convert the file extension to the MIME type
+  if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".json")) return "application/json";
+  return "text/plain";
+}
+
+bool handleFileRead(String path)
+{
+  if(SPIFFS.exists(path))
+  {
+    server.sendHeader("Cache-Control", "max-age=3600", true);
+    String contentType = getContentType(path);
+    File file = SPIFFS.open(path, "r");
+    size_t bytes = server.streamFile(file, contentType, HTTP_GET);
+    file.close();
+    return true;
+  }
+  
+  return false;
+}
+
+
+
+void setup(void) {
+  pinMode(xDRD, OUTPUT);
+  pinMode(xFLT, OUTPUT);
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  SPIFFS.begin();
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+  if(tempSensor.begin())
+  {
+    Serial.println("temp sensor started");
+    tempSensor.setThermocoupleType(MAX31856_TCTYPE_J);
+    if(tempSensor.getThermocoupleType() != MAX31856_TCTYPE_J)
+    {
+      Serial.println("**NOREADBACK**");
+      while(1);
+    }
+    else
+    {
+        Serial.println(tempSensor.getThermocoupleType() == MAX31856_TCTYPE_J);
+    }
+
+    tempSensor.setTempFaultThreshholds(-50.0, 4000.0);
+    tempSensor.setColdJunctionFaultThreshholds(-10,40);
+    tempSensor.setNoiseFilter(MAX31856_NOISE_FILTER_60HZ);
+    sampleData();
+     
+  }
+  else
+  {
+    Serial.println("temp sensor did not start");
+  }
+  
+
+  if (MDNS.begin(STANAME)) {
+    Serial.println("MDNS responder started");
+  }
+
+  pftime::configTime("MDT-6", "0.ca.pool.ntp.org");
+
+  server.on("/", handleRoot);
+  server.on("/data.json", handleDataJson);
+  server.on("/data.js", handleDataJs);
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+
+
+void sampleData()
+{
+  current = (current +1) % BUF_LEN;
+  buffer[current].time = pftime::time(nullptr);
+  buffer[current].outsideTemp =tempSensor.readCJTemperature();
+  buffer[current].insideTemp = tempSensor.readThermocoupleTemperature();
+  
+   uint8_t fault = tempSensor.readFault();
+    
+    if (fault) 
+    {
+      if (fault & MAX31856_FAULT_CJRANGE) Serial.println("Cold Junction Range Fault");
+      if (fault & MAX31856_FAULT_TCRANGE) Serial.println("Thermocouple Range Fault");
+      if (fault & MAX31856_FAULT_CJHIGH)  Serial.println("Cold Junction High Fault");
+      if (fault & MAX31856_FAULT_CJLOW)   Serial.println("Cold Junction Low Fault");
+      if (fault & MAX31856_FAULT_TCHIGH)  Serial.println("Thermocouple High Fault");
+      if (fault & MAX31856_FAULT_TCLOW)   Serial.println("Thermocouple Low Fault");
+      if (fault & MAX31856_FAULT_OVUV)    Serial.println("Over/Under Voltage Fault");
+      if (fault & MAX31856_FAULT_OPEN)    Serial.println("Thermocouple Open Fault"); 
+    }
+
+  
+  Serial.print(buffer[current].outsideTemp);
+  Serial.print(",");
+  Serial.println(buffer[current].insideTemp);
+  
+
+
+}
+
+
+void handleDataJson()
+{
+  handleData(false);
+}
+
+void handleDataJs()
+{
+  handleData(true);
+}
+
+void handleData(bool isJs)
+{
+
+  time_t now = pftime::time(nullptr);
+  char temp[RESP_BUFFER_LEN];
+  int pos = 0;
+  pos = snprintf(temp, RESP_BUFFER_LEN-pos, "%s{labels: [", isJs?"var data=":"");
+    
+  int startFrom = current + 1;
+
+  for(int i = 0 ; i < BUF_LEN; ++i)
+  {
+    struct tm *tm = localtime(&buffer[(i+startFrom)% BUF_LEN].time);
+    pos += snprintf(&temp[pos], RESP_BUFFER_LEN-pos, "%02h:%02m:02s,", 
+    tm->tm_hour, tm->tm_min, tm->tm_sec );
+  }
+  pos += snprintf(&temp[pos], RESP_BUFFER_LEN-pos, "], series: [[");
+
+  for(int i = 0 ; i < BUF_LEN; ++i)
+  {
+    pos += snprintf(&temp[pos], RESP_BUFFER_LEN-pos, "%f,", buffer[(i+startFrom) % BUF_LEN].outsideTemp);
+  }
+
+  pos += snprintf(&temp[pos], RESP_BUFFER_LEN-pos, "],[");
+  for(int i = 0 ; i < BUF_LEN; ++i)
+  {
+    pos += snprintf(&temp[pos], RESP_BUFFER_LEN-pos, "%f,", buffer[(i+startFrom) % BUF_LEN].insideTemp);
+  }
+  
+
+  pos += snprintf(&temp[pos], RESP_BUFFER_LEN-pos, "]]}%s", isJs?";":"");
+
+
+
+  server.sendHeader("Cache-Control", "no-cache", true);
+  server.send(200, isJs?"application/js":"application/json", temp);
+}
+
+void loop(void) 
+{
+  static unsigned long last=0;
+  MDNS.update();
+  server.handleClient();
+  unsigned long now = millis();
+  if(now - last > SAMPLE_PERIOD)
+  {
+    last = now;
+    sampleData();
+  }
+ 
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # KilnLogger
 InternetOfKilns: A simple ESP8266duino based kiln or bbq temperature logger
+This is just a toy for creating a live graph of the temperature of a backyard raku kiln or other hot object that you can access from your phone with a web browser.
+
+## Pre-Requisites
+* An ESP8266 based board
+* An Adafruit MAX31856 Breakout
+* Kiln or other thermocouple.  The MAX31846 supports a wide range
+* The Arduino IDE
+* The ESP8266 boards package plugin for the Arduino IDE
+* Adafruit MAX31856 libraries installed from the library manager and all pre-requisites
+* The ESP8266 sketch data upload plugin for the arduino IDE
+* the ESPPerfectTime library (libraries manager)
+
+## External Requirements/Licenses included with the package
+* Includes chartist-js: https://gionkunz.github.io/chartist-js/  (MIT License)
+
+## TODO's if you want to use this
+* Set your own WIFI SID and passwords STASSID, STAPSK, and STANAME
+* Make sure the thermocouple type set in setup matches your thermocouple.  Typical kiln thermocouples are type K.
+* Pin mappings:  This sketch uses bitbanged SPI because I couldn't figure out the hardware yet.  See xCS, xSDI, xSDO, xSCK, xFLT, xDRD.
+* Solder the connections and keep them short.  Attach the thermocouple.
+* Make sure to use the ESP8266 sketch data upload plugin to copy the files from data folder into the SPIFFS filesystem on the ESP8266
+
+## #include <stddisclaimer.h>
+* No safety or reliability testing has been completed.
+* No safety certifications have been sought or received
+* The author(s) accept no liability for use.
+* You are responsible for monitoring dangerous equipment in a safe manor, and this isn't a safe or reliable manor.
+* No warranty is expressed or implied for fitness of use for any purpose.  
+* Use at your own risk
+* Seriously, don't burn your self, your house or other buildings, animals, family, neighborhood, or city because you trusted your build of this device and it's software to tell you something about the state of a kiln or BBQ or any other hot thing you might want to monitor.
